3455523e24fd164dcd403dff693aec007f45bdad
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-05-03 23:47:32 +0530

    addingtwo teams

--- app/src/main/java/com/example/jaspreetsingh/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/courtcounter/MainActivity.java
@@ -6,33 +6,63 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int score;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-        public void threePoint  (View view)    {
-            score = score+3;
-            displayForTeamA(score);
-        }
-    public void twoPoint    (View view) {
-        score = score + 2;
-        displayForTeamA(score);
+    /*
+    ** Increase score for Team A
+     */
+    public void threePointA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
-    public void freeThrow    (View view) {
-        score = score + 1;
-        displayForTeamA(score);
+
+    public void twoPointA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
-        /**
-         * Displays the given score for Team A.
-         */
-        public void displayForTeamA(int score) {
-            TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-            scoreView.setText(String.valueOf(score));
-        }
 
+    public void freeThrowA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
 
+    /*
+    ** Increase score for Team B
+     */
+    public void threePointB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void twoPointB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given scoreTeamA for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreTeamAView = (TextView) findViewById(R.id.team_a_score);
+        scoreTeamAView.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the given scoreTeamB for Team B
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreTeamBView = (TextView) findViewById(R.id.team_b_score);
+        scoreTeamBView.setText(String.valueOf(score));
+    }
 
 }
